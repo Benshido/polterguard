1baf77a719ea9d38dacc261e56d2b5591010d650
Valentino <valentino136@gmail.com>
2023-03-09 15:29:06 +0100

    Fixed merge errors

--- Polterguard/Assets/Roderick/InteractionDoor2.cs
+++ Polterguard/Assets/Roderick/InteractionDoor2.cs
@@ -3,18 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interaction : MonoBehaviour
+public class InteractionDoor2 : MonoBehaviour
 {
     // Start is called before the first frame update
 
     public float interactive = 0;
     public bool Interacted = false;
     public GameObject InteractText;
-    public Animator animator;
-    public GameObject PlayerCam;
-    public GameObject DoorCam;
-    public GameObject Player;
-    public GameObject Levelexit;
 
 
     private void OnTriggerEnter(Collider other)
@@ -40,7 +35,6 @@ public class Interaction : MonoBehaviour
 
     void Start()
     {
-        Levelexit.SetActive(false);
     }
 
     // Update is called once per frame
@@ -50,24 +44,10 @@ public class Interaction : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.F))
             {
-                animator.enabled = true;
+
                 Interacted = true;
                 InteractText.SetActive(false);
-                PlayerCam.SetActive(false);
-                DoorCam.SetActive(true);
-                Player.GetComponent<FirstPersonController>().enabled = false;
-                StartCoroutine(CameraTimer(3));
-                this.GetComponent<MeshRenderer>().enabled = false;
-                Levelexit.SetActive(true);
             }
         }
-    }
-    IEnumerator CameraTimer(float seconds)
-    {
-        yield return new WaitForSeconds(seconds);
-        PlayerCam.SetActive(true);
-        DoorCam.SetActive(false);
-        Player.GetComponent<FirstPersonController>().enabled = true;
-        this.gameObject.SetActive(false);
     }
 }
---
+++ Polterguard/Assets/Scripts/Interaction.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float interactive = 0;
+    public bool Interacted = false;
+    public GameObject InteractText;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (this.enabled == true)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                interactive = 1;
+                InteractText.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            interactive = 0;
+            InteractText.SetActive(false);
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (interactive == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                Interacted = true;
+            }
+        }
+    }
+}
